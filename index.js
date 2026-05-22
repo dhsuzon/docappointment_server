6b54058921f5083a45cafca2715b31628659cc14
Mohammad Anwar <anwar.hussen.pro@gmail.com>
2026-05-22 23:31:07 +0600

    chore: update cors allowed origins for production

--- index.js
+++ index.js
@@ -6,8 +6,23 @@ const { createRemoteJWKSet, jwtVerify } = require("jose-cjs");
 
 
 const app = express();;
-app.use(cors());
 app.use(express.json());
+const allowedOrigins = [
+    'http://localhost:3000',
+    'https://docappoint-two.vercel.app'
+];
+app.use(cors({
+    origin: function(origin, callback) {
+        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+            callback(null, true);
+        } else {
+            callback(new Error('Not allowed by CORS'));
+        }
+    },
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With', 'Accept']
+}));
 const port = process.env.PORT || 9000;
 const uri = process.env.DB_URL;
 
